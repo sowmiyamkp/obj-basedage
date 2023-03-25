818f938f8ebec06a12c07f715ba3688eb93a9dbc
sowmiyamkp <sowmiyamkp1305@gmail.com>
2023-03-24 17:19:46 -0700

    add obj-basedage

---
+++ main/main.js
@@ -0,0 +1,45 @@
+let employees = [
+
+{
+
+firstName: 'John',
+
+lastName: 'Doe',
+
+age: 27,
+
+joinedDate: 'December 15, 2017'
+
+},
+
+{
+
+firstName: 'Ana',
+
+lastName: 'Rosy',
+
+age: 25,
+
+joinedDate: 'January 15, 2019'
+
+},
+
+{
+
+firstName: 'Zion',
+
+lastName: 'Albert',
+
+age: 30,
+
+joinedDate: 'February 15, 2011'
+
+}
+
+];
+
+let person=employees.sort(function(a,b)
+	{
+	return a.age-b.age;
+	});
+	console.log(person);
